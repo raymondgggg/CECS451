e49cc2fc5750bc6f2f15b2e8282a97490d2175ae
Raymond <raymond13gggg@gmail.com>
2022-02-18 21:10:10 -0800

    structure

--- Assignment3/genetic.py
+++ Assignment3/genetic.py
@@ -1,34 +1,41 @@
 from board import Board
 
 """pass in the dna encoded ordering of the queens 
-   and get the the fitness of the board setup"""
+   and get the the fitness of the board setup step 3"""
 def encoded_fitness(gene):
     """Raymond"""
 
 """pass in a gene's fitness along with a list of the other fitnesses and receive 
-   a normalized value for gene fitness"""
+   a normalized value for gene fitness step 4"""
 def normalize_fitness(gene, geneFitnesses):
     """Raymond2"""
 
 """function that uses normalized fitness value as chance of selection
-   Note: needs to take in the number of genes, and the normalized values to determine selection"""
+   Note: needs to take in the number of genes, and the normalized values to determine selection step 5"""
 def selection():
     """RayRay"""
 
-""""""
+""" step 6"""
 def pairs():
     """Raymond"""
 
+"""step 7"""
+def cross_over():
+    """Raymond """
+    
+"""step 8"""
+def mutation():
+    """raymond"""
 
 """pass in the dna encoded ordering of queens 
-   and get the board object"""
+   and get the board object step 2"""
 def gene_to_board(gene):
     """Raymond1""" 
 
-
-
-
 """function that takes a board object and converts it to a string representation 
-   for further genetic programming """
+   for further genetic programming step 1"""
 def board_to_gene(board):
     """Raymond"""
+
+if __name__ == "__main__":
+    """raymond"""
